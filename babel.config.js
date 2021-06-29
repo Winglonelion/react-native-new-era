53d80245eb6681aeab38a4c4fdc47a9869c5f473
Thuan <nguyenbuiducthuan@gmail.com>
2021-06-29 17:51:59 +0700

    config compiler

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // 'react-native-paper/babel',
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['./src'],
+      },
+    ],
+  ],
 };
