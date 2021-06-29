a1f1f6cbc3f1db7d5fb5ff095fcfa6ecd164e1fb
Thuan <nguyenbuiducthuan@gmail.com>
2021-06-29 17:51:19 +0700

    config linter and formater

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  env: {
+    // browser: true,
+    // es2020: true,
+    // jest: true,
+    // es6: true,
+    // node: true,
+  },
+  globals: {
+    // __jest: true,
+    // __DEV__: true,
+    // GLOBAL: true,
+  },
+  rules: {
+    // 'no-console': 'warn',
+  },
 };
