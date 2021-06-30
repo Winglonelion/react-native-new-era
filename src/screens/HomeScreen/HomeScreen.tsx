dc87c22d0bfa61299a8990ee845d9c63d2b24669
Thuan <nguyenbuiducthuan@gmail.com>
2021-06-30 11:07:28 +0700

    add Blank screens

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+
+import CommonStyles from 'theme/CommonStyles';
+// import styles from './HomeScreen.styles';
+
+type PropTypes = {};
+
+const HomeScreen: React.FC<PropTypes> = () => {
+  return (
+    <View style={CommonStyles.container}>
+      <SafeAreaView style={CommonStyles.flex1} />
+    </View>
+  );
+};
+
+export default HomeScreen;
